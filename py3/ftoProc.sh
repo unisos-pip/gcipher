7373f1d2f8753c3099cd3e1733ba5a144e45a3f1
Mohsen Banan <form@mohsen.banan.1.byname.net>
2022-02-27 14:44:34 -0800

    bx-gitRepos-auto

--- py3/ftoProc.sh
+++ py3/ftoProc.sh
@@ -57,10 +57,8 @@ _CommentEnd_
 ####+BEGIN: bx:dblock:ploneProc:bash:nodesList :types ""
 # {{{ DBLOCK-nodesList
 nodesList="
-dist
-unisos
-unisos.gcipher.egg-info
 bin
+unisos
 "
 # }}} DBLOCK-nodesList
 ####+END:
